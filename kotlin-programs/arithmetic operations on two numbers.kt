dd01cb3b8292b837bf00255617666fcabf535c8b
GSV13719833 <116171924+GSV13719833@users.noreply.github.com>
2022-10-30 05:27:27 +0530

    Create arithmetic operations on two numbers.kt

---
+++ kotlin-programs/arithmetic operations on two numbers.kt
@@ -0,0 +1,20 @@
+package com.includehelp.basic
+
+// Main Method Entry Point of Program
+fun main(args:Array<String>){
+    val a = 13
+    val b = 5
+
+    val sum  = a + b  // Perform Addition
+    val sub  = a - b  // Perform Subtraction
+    val muti = a * b  // Perform Multiplication
+    val div  = a / b  // Perform Division
+    val rem  = a % b  // Perform remainder
+
+    // Print  on Console
+    println("Addison of $a and $b is       : $sum")
+    println("Subtraction of $a and $b is   : $sub")
+    println("Multiplication of $a and $b is: $muti")
+    println("Division of $a and $b is      : $div")
+    println("Remainder of $a and $b is     : $rem")
+}
